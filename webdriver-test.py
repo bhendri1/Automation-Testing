eef90b23c28d2bff8dc2f8e4d4a76cc08a882aa1
Bhendri1 <brettryanh04@gmail.com>
2025-03-18 16:23:44 -0400

    initial code

---
+++ webdriver-test.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+USER_NAME = 'wyzant@gmail.com'
+PASSWORD = 'Wyzant123'
+
+HOME_PAGE_URL = 'https://thinking-tester-contact-list.herokuapp.com/'
+LOGIN_URL = 'https://thinking-tester-contact-list.herokuapp.com/login'
+SIGN_UP_URL = 'https://thinking-tester-contact-list.herokuapp.com/addUser'
+CONTACT_LIST_LANDING_PAGE = 'https://thinking-tester-contact-list.herokuapp.com/contactList'
+
+EMAIL_IN_USE_ERROR_MSG = 'Email address is already in use'
+
+
+driver = webdriver.Chrome()
+driver.get(HOME_PAGE_URL)
+
+driver.find_element(By.ID, 'signup').click()
+driver.implicitly_wait(1)
+
+assert(driver.current_url ==  SIGN_UP_URL)
+
+assert(len(driver.find_elements(By.ID, 'error')) == 1)
+errorSpan = driver.find_element(By.CSS_SELECTOR, 'span#error')
+
+assert(len(errorSpan.text) == 0)
+
+driver.find_element(By.ID, 'firstName').send_keys('Name')
+driver.find_element(By.ID, 'lastName').send_keys('Johnson')
+driver.find_element(By.ID, 'email').send_keys(USER_NAME)
+driver.find_element(By.ID, 'password').send_keys(PASSWORD)
+
+driver.find_element(By.ID, 'submit').click()
+driver.implicitly_wait(1)
+
+if driver.current_url == SIGN_UP_URL:
+    # user already exists
+    assert(errorSpan.text == EMAIL_IN_USE_ERROR_MSG)
+    driver.find_element(By.CSS_SELECTOR, 'button#cancel').click()
+    assert(driver.current_url == LOGIN_URL)
+    driver.find_element(By.ID, 'email').send_keys(USER_NAME)
+    driver.find_element(By.ID, 'password').send_keys(PASSWORD)
+    driver.find_element(By.ID, 'submit').click()
+    driver.implicitly_wait(1)
+
+assert(driver.current_url  == CONTACT_LIST_LANDING_PAGE)
+
+driver.quit()
